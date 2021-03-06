73ef970468120821435ebb0fc8f1a776da8cf600
Gretten <shhworkmail@yandex.ru>
2021-03-06 21:52:32 +0300

    front init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Super Gallery</h1>
+  <Main />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './views/Main/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main
   }
 }
 </script>
---
+++ src/views/Main/Main.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="main-container">
+        <div class="card-container" v-for="image in images" :key="image.id">
+            <img class="card-image" :src="image.link" :alt="image.header">
+            <span class="card-title">{{ image.header }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                images: [
+                    {   
+                        id: 1,
+                        header: 'Albatroz',
+                        link: [require("@/assets/albatroz.jpg")],
+                    },
+                    {   
+                        id: 2,
+                        header: 'Cat',
+                        link: [require("@/assets/cat.jpg")],
+                    },
+                     {   
+                        id: 3,
+                        header: 'Bird',
+                        link: [require("@/assets/bird.jpg")],
+                    },
+                                        {   
+                        id: 1,
+                        header: 'Albatroz',
+                        link: [require("@/assets/albatroz.jpg")],
+                    },
+                    {   
+                        id: 2,
+                        header: 'Cat',
+                        link: [require("@/assets/cat.jpg")],
+                    },
+                     {   
+                        id: 3,
+                        header: 'Bird',
+                        link: [require("@/assets/bird.jpg")],
+                    },
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .main-container {
+        display: flex;
+        margin: 10px;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+    .card-container {
+        display: flex;
+        flex-direction: column;
+        max-height: 380px;
+        margin: 10px;
+        box-shadow: 0 0 10px black;
+        padding: 20px;
+        border-radius: 10px;
+    }
+    .card-image {
+        max-height: 350px;
+    }
+    .card-title {
+        font-weight: 500;
+        font-size: 25px;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+</style>
+ 
